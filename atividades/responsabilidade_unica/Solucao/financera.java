cf47d7882bb13c382a1a99286719ee8f3e14d828
Nadson Nascimento <Nadsontvw@gmail.com>
2023-10-04 11:25:14 -0300

    financera,rh

---
+++ atividades/responsabilidade_unica/Solucao/financera.java
@@ -0,0 +1,6 @@
+public class financera {
+
+    public void gerarFatura() {
+        // Código para gerar uma fatura para o cliente
+    }
+}
---
+++ atividades/responsabilidade_unica/Solucao/rh.java
@@ -0,0 +1,10 @@
+public class rh {
+
+    public void salvarClienteNoBancoDeDados() {
+        // Código para salvar os detalhes do cliente no banco de dados
+    }
+    
+    public void enviarEmailConfirmacao() {
+        // Código para enviar um email de confirmação para o cliente
+    }
+}
